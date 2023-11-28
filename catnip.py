36445a0b07964743e02f652541c5dde78eeb173d
Dario Presutti <dariopresde@protonmail.com>
2023-11-28 09:07:44 +0100

    try hooh to rephrase message

--- catnip.py
+++ catnip.py
@@ -6,14 +6,21 @@ from .feeder import get_news
 @tool
 def get_latest_news(tool_input, cat):
     """"use this tool to respond to requests like 'give me good news' ecc.."""
-    n = get_news()
+    try:
+        n = get_news()
+        return n
+    except Exception as e:
+        print(e)
+        return "Oops, nothing good today!"  
 
-    return cat.llm(
-        f"""
-        These are a list of articles.
-        {print(n)}
 
-        Leave in this list ONLY the articles that bring joy.
-        """
-    )
+@hook(priority=1)
+def before_cat_sends_message(final_output, cat):
+    
+    print(final_output)
+    prompt = 
+        "Filter from the list all message that are not a extremely POSITIVE \n" +
+        f"This is the list: {final_output}"
+    
+    return cat.llm(prompt)
 
