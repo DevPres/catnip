966e9c4529fb2d11b3508acb91c44b1944ad2e05
Dario Presutti <dariopresde@protonmail.com>
2023-11-28 19:33:01 +0100

    format response

--- catnip.py
+++ catnip.py
@@ -19,6 +19,7 @@ def get_latest_news(tool_input, cat):
 def before_cat_sends_message(final_output, cat):
     prompt = f'This is the news list: {final_output["content"]}. \
     Filter from the list all article that are extremely POSITIVE. \
+    ALWAYS response with all the article filtered. \
     format response as bullet pointed list'
     final_output["content"] = cat.llm(prompt)
 
