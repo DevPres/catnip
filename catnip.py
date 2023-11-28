73ac88a7dab6de375f44cfd982ddc6061273376c
Dario Presutti <dariopresde@protonmail.com>
2023-11-28 18:58:23 +0100

    trying to randomize source

--- catnip.py
+++ catnip.py
@@ -1,24 +1,27 @@
 from cat.mad_hatter.decorators import tool, hook, plugin
-from datetime import datetime, date
 from .feeder import get_news
 
 
 @tool
 def get_latest_news(tool_input, cat):
-    """"use this tool to respond to requests like 'give me good news'. Input is always none """
+    """Use this tool to respond to requests like 'give me good news'. \
+    Input is always none."""
+
     try:
         n = get_news()
         return str(n)
     except Exception as e:
         print(e)
-        return "Oops, nothing good today!"  
+        return "Oops, nothing good today!"
 
 
 @hook(priority=1)
 def before_cat_sends_message(final_output, cat):
-    
-    prompt =f'This is the news list: {final_output["content"]}. Filter from the list all message that are extremely POSITIVE, and format as bullet pointed list'
-    final_output["content"] = cat.llm(prompt);     
-    
-    return final_output
+    """Filter."""
 
+    prompt = f'This is the news list: {final_output["content"]}. \
+    Filter from the list all message that are extremely POSITIVE, \
+    format as bullet pointed list'
+    final_output["content"] = cat.llm(prompt)
+
+    return final_output
--- feeder.py
+++ feeder.py
@@ -4,25 +4,23 @@ import feedparser
 def get_news():
     news_list = []
     for category in CATEGORIES:
-        for news in get_news_by_category(category):
-            news_list.append(news)
+        catgeory_news = get_news_by_category(category)
+        for i in range(5):
+            news_list.append(catgeory_news[i])
 
     return news_list
 
+
 def get_news_by_category(category):
-    url = f'https://rss.nytimes.com/services/xml/rss/nyt/{category}.xml' 
+    url = f"https://rss.nytimes.com/services/xml/rss/nyt/{category}.xml"
     d = feedparser.parse(url)
     news_list = []
-    for news in d['entries']:
+    for news in d["entries"]:
         news_list.append({
-            'Description': news['description'],
+            "Description": news["description"],
+            "Link": news["link"]
         })
     return news_list
 
-CATEGORIES = [
-    'Climate',
-    'Space',
-    'Technology',
-    'Science'
-]
 
+CATEGORIES = ["Climate", "Space", "Technology", "Science" "Arts", "Music"]
